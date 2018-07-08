b12a3cd42f708ae42f7ff731aec78607a735a096
terribledevice <longlost@gmail.com>
2018-07-08 11:03:46 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Movie database app</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<h1>Welcome to my movie database app</h1>
+<p>The goal is to create a simple app that I can use to track which movies I own, where they're located, and add/remove more movies.</p>
+
+</body>
+</html>
