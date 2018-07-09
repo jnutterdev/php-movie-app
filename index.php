d42e9ec6616d78cf435ca5557b2e5c1431d8af18
terribledevice <longlost@gmail.com>
2018-07-08 21:58:57 -0400

    started on login script

--- index.php
+++ index.php
@@ -1,15 +1,70 @@
-<?php ?>
+<?php require 'resources/login.php'?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Movie database app</title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-<h1>Welcome to my movie database app</h1>
-<p>The goal is to create a simple app that I can use to track which movies I own, where they're located, and add/remove more movies.</p>
+  <head>
+    <meta charset="utf-8" content="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movies app</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <link rel="stylesheet" type="text/css" href="static/css/main.css">
+    <script type="text/javascript" src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Movie collection
+      </h1>
+      <p class="subtitle">
+        This page is designed to collect and display my movie collections.
+      </p>
 
-</body>
+    <div class="columns is-desktop main-table">
+  		<div class="column one">Movies</div>
+  		<div class="column one">TV</div>
+	</div>
+ </div>
+  </section>
+  <section class="section two">
+		<div class="tile">
+		<h2>Movie section</h2>
+		</div>
+		<form action="movie-logic.php" method="POST" accept-charset="utf-8">
+			<div class="columns">
+				<div class="column">
+					<input class="input" type="text" placeholder="Title">
+				</div>
+				<div class="column">
+					<input class="input" type="text" placeholder="Year">
+				</div>
+				<div class="column is-one-fifth">
+					<input class="input" type="text" placeholder="Genre">
+				</div>
+				<div class="column is-one-fifth">
+					<input class="input" type="text" placeholder="Location">
+				</div>
+				<div class="column is-one-fifth">
+					<input class="input" type="text" placeholder="Format">
+				</div>
+				<div class="column is-one-fifth">
+					<button class="button">Submit</button>
+				</div>
+			</div>
+		</form>
+		<div class="columns is-desktop two list">
+			<div class="column one ">Title</div>
+  			<div class="column one">Year</div>
+  			<div class="column one">Genre</div>
+  			<div class="column one">Format</div>
+  			<div class="column one">Location</div>
+		</div>
+  </section>
+ <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Copyright 2018</strong> by <a href="https://withonlyamap.github.io">John Nutter</a>.
+    </p>
+  </div>
+</footer>
+  </body>
 </html>
---
+++ scripts/movie-logic.php
@@ -0,0 +1,7 @@
+<?php
+
+$movie_title    = $_POST['movie_title'];
+$movie_year     = $_POST['movie_year'];
+$movie_genre    = $_POST['movie_genre'];
+$movie_location = $_POST['movie_location'];
+$movie_format   = $_POST['movie_format'];
