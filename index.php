9fcd4629dd62080d68c80cbfbdde53f3307cecc0
terribledevice <longlost@gmail.com>
2019-01-31 08:47:03 -0500

    added bread crumbs placeholder

--- index.php
+++ index.php
@@ -48,15 +48,34 @@
 <?php include 'scripts/getmovies.php';?>
 </table>
 <div class="field">
-  <div class="control">
-    <div class="select is-small">
-      <select>
-        <option>25</option>
-        <option>50</option>
-         <option>100</option>
-      </select>
+      <div class="control">
+        <div class="select is-small">
+          <select>
+            <option>25</option>
+            <option>50</option>
+            <option>100</option>
+          </select>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  </article>
-<?php include 'footer.php'?>
+</article>
+  <article>
+      <div class="column is-three-quarters container">
+        <nav class="pagination is-small" role="navigation" aria-label="pagination">
+          <a class="pagination-previous">Previous</a>
+          <a class="pagination-next">Next page</a>
+          <ul class="pagination-list">
+            <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
+            <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+            <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
+            <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+          </ul>
+        </nav>
+      </div>
+      <?php include 'footer.php'?>
+    </article>
+
+
